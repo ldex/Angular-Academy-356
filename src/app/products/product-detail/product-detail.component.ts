98761de8a9e8d2c95571157bcbca49090456b910
ldex <laurent.duveau+git@gmail.com>
2023-03-23 14:58:35 -0400

    Suppression de produit

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, Input, OnDestroy } from '@angular/core'
-import { ActivatedRoute } from '@angular/router'
+import { ActivatedRoute, Router } from '@angular/router'
 import { Observable, Subscription } from 'rxjs'
 import { Product } from 'src/app/models/product.interface'
 import { ProductService } from 'src/app/services/product.service'
@@ -15,9 +15,23 @@ export class ProductDetailComponent implements OnDestroy {
   product: Product;
   sub: Subscription;
 
+  deleteProduct(id: number) {
+    this
+      .productService
+      .deleteProduct(id)
+      .subscribe(
+        () => {
+          console.log('Product deleted on server.');
+          this.productService.initProducts();
+          this.router.navigateByUrl('/products');
+        }
+      )
+  }
+
   constructor(
     private activatedRoute: ActivatedRoute,
-    private productService: ProductService
+    private productService: ProductService,
+    private router: Router
   ) {
     const id = activatedRoute.snapshot.params.id
 
