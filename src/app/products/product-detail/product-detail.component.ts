cdfd68c70b94f1481f17cf664eee2013e123d40b
ldex <laurent.duveau+git@gmail.com>
2023-03-23 13:10:04 -0400

    usage de Unsubscribe

--- src/app/products/product-detail/product-detail.component.ts
+++ src/app/products/product-detail/product-detail.component.ts
@@ -1,28 +1,42 @@
-import { Component, Input } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { Product } from 'src/app/models/product.interface';
-import { ProductService } from 'src/app/services/product.service';
+import { Component, Input, OnDestroy } from '@angular/core'
+import { ActivatedRoute } from '@angular/router'
+import { Observable, Subscription } from 'rxjs'
+import { Product } from 'src/app/models/product.interface'
+import { ProductService } from 'src/app/services/product.service'
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnDestroy {
 
-  @Input() product: Product;
+  product$: Observable<Product>
+  product: Product;
+  sub: Subscription;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private productService: ProductService
   ) {
-    const id = activatedRoute.snapshot.params.id;
+    const id = activatedRoute.snapshot.params.id
 
-    productService
-      .getProductById(id)
-      .subscribe(
-        result => this.product = result
-      )
+    // Avec pipe async
+    this.product$ = productService.getProductById(id)
+
+    // Avec subscribe/unsubscribe
+    // this.sub = new Subscription();
+
+    // this.sub.add(
+    //   productService.getProductById(id)
+    //     .subscribe(
+    //       result => this.product = result
+    //     )
+    )
+  }
+
+  ngOnDestroy(): void {
+    this.sub.unsubscribe();
   }
 
 }
